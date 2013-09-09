24f23f6568bc0ca51428a5daa2c7c41fa4749677
Tobias Friese <tobifriese@gmail.com>
2013-09-09 14:27:42 +0200

    updates Toby_HTML_Form

--- html/Toby_HTML_Form.class.php
+++ html/Toby_HTML_Form.class.php
@@ -2,6 +2,7 @@
 
 class Toby_HTML_Form
 {
+    /* variables */
     private $name;
     private $action;
     private $method; 
@@ -11,13 +12,16 @@ class Toby_HTML_Form
     
     private $omitFormTag                    = false;
     
-    public static $METHOD_GET               = 'GET';
-    public static $METHOD_POST              = 'POST';
+    /* constants */
+    const METHOD_GET                        = 'GET';
+    const METHOD_POST                       = 'POST';
     
-    public static $ELEMENT_INPUT_TEXT       = 'inputText';
-    public static $ELEMENT_INPUT_PASSWORD   = 'inputPassword';
-    public static $ELEMENT_INPUT_HIDDEN     = 'inputHidden';
-    public static $ELEMENT_TEXTAREA         = 'textarea';
+    const ELEMENT_INPUT_TEXT                = 'inputText';
+    const ELEMENT_INPUT_PASSWORD            = 'inputPassword';
+    const ELEMENT_INPUT_HIDDEN              = 'inputHidden';
+    const ELEMENT_TEXTAREA                  = 'textarea';
+    const ELEMENT_CHECKBOX                  = 'checkbox';
+    const ELEMENT_CUSTOM                    = 'custom';
     
     function __construct($name, $action, $method = 'POST', $attr = null)
     {
@@ -30,7 +34,7 @@ class Toby_HTML_Form
     public function addTextInput($name, $value = '', $attr = null)
     {
         $this->elements[] = array(
-            'type'          =>  self::$ELEMENT_INPUT_TEXT,
+            'type'          =>  self::ELEMENT_INPUT_TEXT,
             'name'          => $name,
             'value'         => $value,
             'attr'          => $attr
@@ -43,7 +47,7 @@ class Toby_HTML_Form
     public function addPasswordInput($name, $value = '', $attr = null)
     {
         $this->elements[] = array(
-            'type'          =>  self::$ELEMENT_INPUT_PASSWORD,
+            'type'          =>  self::ELEMENT_INPUT_PASSWORD,
             'name'          => $name,
             'value'         => $value,
             'attr'          => $attr
@@ -56,7 +60,7 @@ class Toby_HTML_Form
     public function addHiddenInput($name, $value = '', $attr = null)
     {
         $this->elements[] = array(
-            'type'          =>  self::$ELEMENT_INPUT_HIDDEN,
+            'type'          =>  self::ELEMENT_INPUT_HIDDEN,
             'name'          => $name,
             'value'         => $value,
             'attr'          => $attr
@@ -69,9 +73,35 @@ class Toby_HTML_Form
     public function addTextArea($name, $value = '',  $attr = null)
     {
         $this->elements[] = array(
-            'type'          =>  self::$ELEMENT_TEXTAREA,
+            'type'          =>  self::ELEMENT_TEXTAREA,
             'name'          => $name,
             'value'         => $value,
+            'attr'          => $attr
+        );
+
+        // return
+        return $this;
+    }
+    
+    public function addCheckBox($name, $checked = false, $attr = null)
+    {
+        $this->elements[] = array(
+            'type'          =>  self::ELEMENT_CHECKBOX,
+            'name'          => $name,
+            'checked'       => $checked,
+            'attr'          => $attr
+        );
+
+        // return
+        return $this;
+    }
+    
+    public function addCustomElement($tagName, $content = null, $attr = null)
+    {
+        $this->elements[] = array(
+            'type'          =>  self::ELEMENT_CUSTOM,
+            'tagName'       => $tagName,
+            'content'       => $content,
             'attr'          => $attr
         );
 
@@ -87,11 +117,11 @@ class Toby_HTML_Form
         {
             switch($elm['type'])
             {
-                case self::$ELEMENT_INPUT_TEXT:
+                case self::ELEMENT_INPUT_TEXT:
                     
                     $tag = new Toby_HTML_Tag('input');
                     
-                    $tag->addAttributes(isset($elm['attr']) ? $elm['attr'] : null);
+                    if(isset($elm['attr'])) $tag->addAttributes($elm['attr']);
                     
                     $tag->addAttribute('type', 'text')
                         ->addAttribute('name', "$this->name[{$elm['name']}]")
@@ -101,11 +131,11 @@ class Toby_HTML_Form
                     
                     break;
                 
-                case self::$ELEMENT_INPUT_PASSWORD:
+                case self::ELEMENT_INPUT_PASSWORD:
                     
                     $tag = new Toby_HTML_Tag('input');
                     
-                    $tag->addAttributes(isset($elm['attr']) ? $elm['attr'] : null);
+                    if(isset($elm['attr'])) $tag->addAttributes($elm['attr']);
                     
                     $tag->addAttribute('type', 'password')
                         ->addAttribute('name', "$this->name[{$elm['name']}]")
@@ -115,11 +145,11 @@ class Toby_HTML_Form
                     
                     break;
                 
-                case self::$ELEMENT_INPUT_HIDDEN:
+                case self::ELEMENT_INPUT_HIDDEN:
                     
                     $tag = new Toby_HTML_Tag('input');
                     
-                    $tag->addAttributes(isset($elm['attr']) ? $elm['attr'] : null);
+                    if(isset($elm['attr'])) $tag->addAttributes($elm['attr']);
                     
                     $tag->addAttribute('type', 'hidden')
                         ->addAttribute('name', "$this->name[{$elm['name']}]")
@@ -129,14 +159,40 @@ class Toby_HTML_Form
                     
                     break;
                 
-                case self::$ELEMENT_TEXTAREA:
+                case self::ELEMENT_TEXTAREA:
                     
                     $tag = new Toby_HTML_Tag('textarea');
                     
-                    $tag->addAttributes(isset($elm['attr']) ? $elm['attr'] : null);
+                    if(isset($elm['attr'])) $tag->addAttributes($elm['attr']);
                     
                     $tag->addAttribute('name', "$this->name[{$elm['name']}]")
                         ->setContent(isset($_POST[$this->name][$elm['name']]) ? $_POST[$this->name][$elm['name']] : $elm['value']);
+                    
+                    $elements .= $tag->build().NL;
+                    
+                    break;
+                
+                case self::ELEMENT_CHECKBOX:
+                    
+                    $tag = new Toby_HTML_Tag('input');
+                    
+                    if(isset($elm['attr'])) $tag->addAttributes($elm['attr']);
+                    
+                    $tag->addAttribute('type', 'checkbox')
+                        ->addAttribute('name', "$this->name[{$elm['name']}]");
+                        
+                    if($elm['checked'] === true) $tag->addAttribute('checked', 'checked');
+                    
+                    $elements .= $tag->build().NL;
+                    
+                    break;
+                    
+                case self::ELEMENT_CUSTOM:
+                    
+                    $tag = new Toby_HTML_Tag($elm['tagName']);
+                    
+                    if(isset($elm['attr'])) $tag->addAttributes($elm['attr']);
+                    if(isset($elm['content'])) $tag->setContent($elm['content']);
                     
                     $elements .= $tag->build().NL;
                     
