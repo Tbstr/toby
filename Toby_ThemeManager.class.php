de51803b407ba4f98263d7623c9d569468d1ef25
Tobias Friese <tobifriese@gmail.com>
2013-10-08 13:38:03 +0200

    shifts config read functionality to ThemeManager & removes ConfigFileManager

--- Toby_ThemeManager.class.php
+++ Toby_ThemeManager.class.php
@@ -114,7 +114,7 @@ class Toby_ThemeManager
             if($dataFromCache === false)
             {
                 // read & parse
-                self::$themeConfig = Toby_ConfigFileManager::read($configPath, true);
+                self::$themeConfig = self::readConfig($configPath, true);
 
                 // cache
                 if($shmop === true)
@@ -210,5 +210,133 @@ class Toby_ThemeManager
                 }
             }
         }
+    }
+    
+    /* config parsing */
+    public static function readConfig($filePath, $processSections = false)
+    {
+        if(file_exists($filePath))
+        {
+            if(($handle = @fopen($filePath, 'r')) === false) return false;
+            
+            $data = array();
+            
+            if($processSections)
+            {
+                $data['global'] = array();
+                $dataCursor = &$data['global'];
+            }
+            else $dataCursor = &$data;
+            
+            while(($line = fgets($handle)) !== false)
+            {
+                // trim line
+                $line = trim($line);
+                
+                // continue if commented or empty
+                if(strlen($line) === 0) continue;
+                if($line[0] === '#') continue;
+                
+                // proove for section marker
+                if($line[0] === '[' && $line[strlen($line)-1] === ']')
+                {
+                    if($processSections)
+                    {
+                        $sectionName = substr($line, 1, strlen($line) - 2);
+                        $data[$sectionName] = array();
+                        $dataCursor = &$data[$sectionName];
+                    }
+                    
+                    continue;
+                }
+                
+                // get line parts
+                list($keys, $value) = explode('=', $line, 2);
+                $keys = trim($keys);
+                $value = self::readConfigElement(trim($value));
+                
+                // get key parts
+                $counter = 0;
+                $buffer = '';
+                $parts = array();
+                
+                // get keys
+                if(strpos($keys, '[') === false) array_push($parts, $keys);
+                else
+                {
+                    while($counter < strlen($keys))
+                    {
+                        switch($line[$counter])
+                        {
+                            case '[':
+                                if(count($parts) === 0)
+                                {
+                                    array_push($parts, trim($buffer));
+                                    $buffer = '';
+                                }
+                                break;
+
+                            case ']':
+                                array_push($parts, trim($buffer));
+                                $buffer = '';
+                                break;
+
+                            default:
+                                $buffer .= $line[$counter];
+                                break;
+
+                        }
+
+                        $counter++;
+                    }
+                }
+                
+                // apply key parts
+                $counter = 0;
+                $keydataCursor = &$dataCursor;
+                foreach($parts as $part)
+                {
+                    $counter++;
+                    $last = $counter == count($parts);
+
+                    if($part === '')
+                    {
+                        $index = array_push($keydataCursor, ($last ? $value : array()));
+                        $keydataCursor = &$keydataCursor[$index - 1];
+                    }
+                    else
+                    {
+                        if(!isset($keydataCursor[$part]))
+                        {
+                            $keydataCursor[$part] = ($last ? $value : array());
+                        }
+
+                        $keydataCursor = &$keydataCursor[$part];
+                    }
+                }
+            }
+            
+            fclose($handle);
+            return $data;
+        }
+        
+        return false;
+    }
+    
+    private static function readConfigElement($element)
+    {
+        // boolean
+        $toLower = strtolower($element);
+        if($toLower === 'true' || $toLower === 'false') return $toLower === 'true';
+        
+        // number
+        if(is_numeric($element))
+        {
+            if(strpos($element, '.') === false) return (int)$element;
+            else return (float)$element;
+        }
+        
+        // else
+        return $element;
     }
 }
