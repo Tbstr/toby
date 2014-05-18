2de5ce7ecb9825607fc3bf7b2f57d52067bcaf4e
Tobias Friese <tobifriese@gmail.com>
2014-05-18 15:25:58 +0200

    Toby_Utils::printr() identifies non visible native types (null, boolean)

--- Toby_Utils.class.php
+++ Toby_Utils.class.php
@@ -9,7 +9,14 @@ class Toby_Utils
     public static function printr()
     {
         $args = func_get_args();
-        foreach($args as $arg) echo '<pre>'.print_r($arg, true).'</pre>';
+        foreach($args as $arg)
+        {
+            if($arg === null) $arg = 'null';
+            elseif($arg === true) $arg = 'true';
+            elseif($arg === false) $arg = 'false';
+            
+            echo '<pre>'.print_r($arg, true).'</pre>';
+        }
     }
     
     /* debug */
