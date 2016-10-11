a450549c4a831c04b8a1d67fdc59457d83d5c2e0
Tobias Friese <tobifriese@gmail.com>
2016-10-11 13:08:26 +0200

    updates utils

--- Utils/StringUtils.php
+++ Utils/StringUtils.php
@@ -35,7 +35,12 @@ class StringUtils
         // $bytes /= pow(1024, $pow);
         $bytes /= (1 << (10 * $pow));
 
-        return round($bytes, $precision).' '.$units[$pow];
+        // floor by precision
+        $precisionPow = pow(10, $precision);
+        $bytes = floor($bytes * $precisionPow) / $precisionPow;
+        
+        // return
+        return $bytes.' '.$units[$pow];
     }
 
     public static function constrain($string, $length, $ellipsis = true, $revert = false)
@@ -88,8 +93,8 @@ class StringUtils
         // cancellation
         if(empty($mail)) return false;
 
-        // check
-        return (boolean)preg_match('/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,32}$/i', $mail);
+        // check (regexp from http://emailregex.com/)
+        return (boolean)preg_match('/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD', $mail);
     }
 
     public static function validateURL($url)
@@ -98,7 +103,7 @@ class StringUtils
         if(empty($url)) return false;
 
         // check
-        return (boolean)preg_match('/^(https?:\/\/)?([0-9a-z\.-]+)\.([a-z\.]{2,32})([\/\w \.-]*)*\/?$/i', $url);
+        return (boolean)preg_match('/^https?:\/\/([0-9.]+(:\d{1,5})?|(?![0-9 .-])(\.?(-?[0-9a-z]+)+)+\.[a-z]{2,32})(\/[\w .-]+)?\/?(\?(&?\w+(=\w+)?)+)?$/i', $url);
     }
 
     public static function buildPath(array $elements, $separator = '/')
